6fd089e4576463129e9a65bc33b8425b5030e6aa
Juan <75914125+cirocode@users.noreply.github.com>
2026-05-07 23:59:06 -0400

    Add: is-palindrome - submission-11

---
+++ Data Structures & Algorithms/is-palindrome/submission-11.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if (s.length() == 0 || s.length() == 1) {
+            return true;
+        }
+        int leftIndex = 0;
+        int rightIndex = s.length() - 1;
+        s = s.toLowerCase();
+
+        while (leftIndex <= rightIndex) {
+            while (leftIndex < rightIndex && !alphaNum(s.charAt(leftIndex))) {
+                leftIndex++;
+            }
+
+            while (rightIndex > leftIndex && !alphaNum(s.charAt(rightIndex))) {
+                rightIndex--;
+            }
+
+            if (s.charAt(leftIndex) != s.charAt(rightIndex)) {
+                return false;
+            }
+
+            leftIndex++;
+            rightIndex--;
+        }
+
+        return true;
+        
+    }
+
+    public boolean alphaNum(char c) {
+        return (c >= 'A' && c <= 'Z' ||
+                c >= 'a' && c <= 'z' ||
+                c >= '0' && c <= '9');
+    }
+}
